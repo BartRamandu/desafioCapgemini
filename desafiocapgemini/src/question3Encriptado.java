94f6ffdbb04725a4c793eda41cd683fe06992820
BartRamandu <bartmelo_93@hotmail.com>
2022-03-07 03:26:56 -0300

    @BartRamandu [Task: Tentativa de finalizar a 3 questão]

--- desafiocapgemini/src/question3Encriptado.java
+++ desafiocapgemini/src/question3Encriptado.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class question3Encriptado {
@@ -7,12 +8,22 @@ public class question3Encriptado {
 		
 		String frase = "Nada se cria onde tudo se copia";
 		
+		String fraseUnida = frase.replace(" ", "").toLowerCase();
+		String []fraseUnidaArray = fraseUnida.split("(?!^)");
+		System.out.println(Arrays.toString(fraseUnidaArray));//teste
 		int totalLetras = frase.replace(" ","").length();
 		
-		System.out.println(totalLetras);
+		System.out.println(totalLetras);//teste
 
-		double grid = Math.sqrt(totalLetras);
+		double raiz = Math.sqrt(totalLetras);
+		int grid = (int) Math.round(raiz);
+		System.out.println(grid);//teste
+		grid = grid - 1;
 		
+		String [][]matrizCripto = new String[grid][grid];
+		
+		matrizCripto[0][0] = fraseUnidaArray[0];//teste
+		System.out.println(matrizCripto[0][0]);//teste
 		
 	}
 
